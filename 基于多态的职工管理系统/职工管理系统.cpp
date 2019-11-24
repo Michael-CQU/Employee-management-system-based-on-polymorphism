d4cf18c8a3eb66f793aefaf6f8fa577daea84c72
Michael-CQU <Michael-CQU@users.noreply.github.com>
2019-11-24 21:24:47 +0800

    第五次修改

--- 基于多态的职工管理系统/职工管理系统.cpp
+++ 基于多态的职工管理系统/职工管理系统.cpp
@@ -1,81 +1,32 @@
-﻿#include <iostream> 
-using  namespace std;
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
 #include "workerManager.h"
 
-//#include "worker.h"
-//#include "employee.h"
-//#include "manager.h"
-//#include "boss.h"
+int main() {
 
-int main()
-{
-	////���Դ��룺
-	//Worker * worker = NULL;
-	//worker = new Employee(1, "����", 1);
-	//worker->showInfo();
-	//delete worker;
-
-	//测试一下上传效果
-	//进行字体测试
-
-
-	//worker = new Manager(2, "����", 2);
-	//worker->showInfo();
-	//delete worker;
-
-	//worker = new Boss(3, "����", 3);
-	//worker->showInfo();
-	//delete worker;
-
-	//ʵ���������߶���
 	WorkerManager wm;
-
-	int choice = 0; //�����洢�û���ѡ��
-
-	while (true)
+	int choice = 0;
+	while (1)
 	{
-		//����չʾ�˵���Ա����
 		wm.Show_Menu();
-
-		cout << "����������ѡ�� " << endl;
-		cin >> choice; // �����û���ѡ��
-
+		cout << endl << "请输入您的选择:" << endl;
+		cin >> choice;
+		cin.get();
 		switch (choice)
 		{
-		case 0:  //�˳�ϵͳ
-			wm.ExitSystem();
-			break;
-		case 1:  //����ְ��
-			wm.Add_Emp();
-			break;
-		case 2:  //��ʾְ��
-			wm.Show_Emp();
-			break;
-		case 3:  //ɾ��ְ��
-			wm.Del_Emp();
-			break;
-		case 4:  //�޸�ְ��
-			wm.Mod_Emp();
-			break;
-		case 5:  //����ְ��
-			wm.Find_Emp();
-			break;
-		case 6:  //����ְ��
-			wm.Sort_Emp();
-			break;
-		case 7:  //����ĵ�
-			wm.Clean_File();
-			break;
-		default:
-			system("cls"); //����
-			break;
+		case 0:return 0; break;
+		case 1:wm.Add_Emp();  cout << "按回车键继续" << endl;  cin.get(); break;
+		case 2:wm.Del_Emp();  cout << "按回车键继续" << endl;  cin.get(); break;
+		case 3:wm.Show_Emp(); cout << "按回车键继续" << endl;  cin.get(); break;
+		case 4:wm.Mod_Emp();  cout << "按回车键继续" << endl;  cin.get(); break;
+		case 5:wm.Find_Emp(); cout << "按回车键继续" << endl;  cin.get(); break;
+		case 6:wm.Sort_Emp(); cout << "按回车键继续" << endl;  cin.get(); break;
+		case 7:wm.Clean_File();   cout << "按回车键继续" << endl;  cin.get(); break;
 		}
-
+		system("cls");
 	}
 
-
-
 	system("pause");
-
-	return 0;
+	return EXIT_SUCCESS;
 }
